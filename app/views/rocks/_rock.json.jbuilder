323ea1ae90fd66fc987aa1dbee21983811fdbcb4
Kate Hunter <iamkatehunter@gmail.com>
2022-05-25 12:25:38 +1000

    first commit

---
+++ app/views/rocks/_rock.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! rock, :id, :image, :created_at, :updated_at
+json.url rock_url(rock, format: :json)
---
+++ db/migrate/20220517110133_create_rocks.rb
@@ -0,0 +1,9 @@
+class CreateRocks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :rocks do |t|
+      t.text :image
+
+      t.timestamps
+    end
+  end
+end
